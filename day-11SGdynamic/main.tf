f54cc9ddf2403f05765a62417d4e29a9fd0837bd
simonxox <simonchinnu13@gmail.com>
2025-07-18 10:56:51 +0530

    bbb

--- day-11SGdynamic/main.tf
+++ day-11SGdynamic/main.tf
@@ -2,18 +2,14 @@
 data "aws_vpc" "default" {
   default = true
 }
+
 resource "aws_security_group" "web_sg" {
   name        = "web-sg"
   description = "Allow specific ports with CIDR blocks"
   vpc_id      = data.aws_vpc.default.id
 
   dynamic "ingress" {
-    for_each = {
-      22   = "203.0.113.10/32"  # SSH - only your IP
-      80   = "0.0.0.0/0"        # HTTP - open to all
-      443  = "0.0.0.0/0"        # HTTPS - open to all
-      3000 = "10.0.0.0/16"      # App Port - internal only
-    }
+    for_each = var.port_cidr_map
     content {
       from_port   = ingress.key
       to_port     = ingress.key
